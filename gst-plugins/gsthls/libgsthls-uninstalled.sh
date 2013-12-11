cab6ebf43bc54cb49957fa6f14854386b0cca6b3
Matt Snoby <snobym@cisco.com>
2013-12-11 10:27:51 -0500

    Removed files from the build system that are autogenererated so they needed to be removed.  I also cleaned up some of the object files that had been checked in.

--- gst-plugins/gsthls/libgsthls-uninstalled.sh
+++ gst-plugins/gsthls/libgsthls-uninstalled.sh
@@ -2,11 +2,11 @@
 prefix="/opt/test"
 exec_prefix="${prefix}"
 bindir="${exec_prefix}/bin"
-libdir="/home/saravan/Downloads/Cisco/habanero-utils/gst-plugins/gsthls/.libs"
+libdir="/home/snoby/work/habanero/gst-plugins/gsthls/.libs"
 datarootdir="${prefix}/share"
 datadir="${datarootdir}"
 sysconfdir="${prefix}/etc"
-includedir="/home/saravan/Downloads/Cisco/habanero-utils/gst-plugins/gsthls/."
+includedir="/home/snoby/work/habanero/gst-plugins/gsthls/."
 package="libgsthls"
 suffix=""
 
@@ -16,7 +16,7 @@ for option; do case "$option" in --list-all|--name) echo  "libgsthls"
 ;; --requires) echo : ""
 ;; --libs) echo -L${libdir} "" "-L/opt/test/lib -lm -lHls-1.0 -L/opt/test/lib -lm -lHls-1.0"
        :
-;; --cflags) echo -I${includedir} "-ggdb -Wall -O0 -pthread -I/opt/test/include -I/opt/test/include/libHls-1.0 -I/opt/test/include/libHls-1.0/include"
+;; --cflags) echo -I${includedir} "-g -O2 -pthread -I/opt/test/include -I/opt/test/include/libHls-1.0 -I/opt/test/include/libHls-1.0/include"
        :
 ;; --variable=*) eval echo '$'`echo $option | sed -e 's/.*=//'`
 ;; --uninstalled) exit 0 
